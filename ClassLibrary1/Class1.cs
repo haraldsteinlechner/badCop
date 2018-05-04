252ce248a0884c97979a75e74aa635c5950f7ae0
szabo@vrvis.at <szabo@vrvis.at>
2018-05-04 10:37:22 +0200

    import bad funny functions

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public static  class Class1
+    {
+        public static void Funny(this Tuple<double, double> urdar)
+        {
+        }
+
+        public static void Funny(this Tuple<int, int> urdar)
+        {
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open ClassLibrary1
+
+let bla = Typey()
+
+[<EntryPoint>]
+let main argv = 
+
+    //Class1.Funny(bla)
+    bla.Funny()
+    0 // return an integer exit code
